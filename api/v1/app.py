d33efd6195203fedd87c2772dcc1a4bb0b85b342
Abdelnissoul <abdelnissoul205@gmail.com>
2024-05-29 00:56:58 +0100

    dunya

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
-"""creating a flsak app"""
-
-from os import getenv
-from flask import Flask
-from models import storage
-from api.v1.views import app_views
+"""Module containing app setup."""
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-app.register_blueprint(app_views)
+
+@app.errorhandler(404)
+def not_found(error):
+    """Handle 404 errors."""
+    return jsonify({'error': 'Not found'}), 404
+
 
 if __name__ == '__main__':
-    HOST = getenv('HBNB_API_HOST', '0.0.0.0')
-    PORT = int(getenv('HBNB_API_PORT', 5000))
-    app.run(host=HOST, port=PORT, threaded=True)
+    app.run(host='0.0.0.0', port=5000)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,10 +1,19 @@
 #!/usr/bin/python3
-"""Index module to handle the /status route."""
-
+"""Module containing index views."""
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
 
-@app_views.route('/status')
-def api_status():
-    """Returns the status of the API."""
-    return jsonify({"status": "OK"})
+
+@app_views.route('/api/v1/stats', methods=['GET'])
+def get_stats():
+    """Retrieve the number of each object type."""
+    stats = {
+        'amenities': storage.count('Amenity'),
+        'cities': storage.count('City'),
+        'places': storage.count('Place'),
+        'reviews': storage.count('Review'),
+        'states': storage.count('State'),
+        'users': storage.count('User')
+    }
+    return jsonify(stats)
