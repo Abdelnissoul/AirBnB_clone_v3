60e73b106d6c38f94aa65afba0923bde680d7dea
Abdelnissoul <abdelnissoul205@gmail.com>
2024-05-28 18:19:34 +0100

    adding get and count methods to DBStorage and FileStorage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -21,7 +21,7 @@ classes = {"Amenity": Amenity, "City": City,
 
 
 class DBStorage:
-    """interaacts with the MySQL database"""
+    """Interacts with the MySQL database"""
     __engine = None
     __session = None
 
@@ -41,7 +41,7 @@ class DBStorage:
             Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
-        """query on the current database session"""
+        """Query on the current database session"""
         new_dict = {}
         for clss in classes:
             if cls is None or cls is classes[clss] or cls is clss:
@@ -49,40 +49,53 @@ class DBStorage:
                 for obj in objs:
                     key = obj.__class__.__name__ + '.' + obj.id
                     new_dict[key] = obj
-        return (new_dict)
+        return new_dict
 
     def new(self, obj):
-        """add the object to the current database session"""
+        """Add the object to the current database session"""
         self.__session.add(obj)
 
     def save(self):
-        """commit all changes of the current database session"""
+        """Commit all changes of the current database session"""
         self.__session.commit()
 
     def delete(self, obj=None):
-        """delete from the current database session obj if not None"""
+        """Delete from the current database session obj if not None"""
         if obj is not None:
             self.__session.delete(obj)
 
     def reload(self):
-        """reloads data from the database"""
+        """Reloads data from the database"""
         Base.metadata.create_all(self.__engine)
         sess_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
         Session = scoped_session(sess_factory)
         self.__session = Session
 
     def close(self):
-        """call remove() method on the private session attribute"""
+        """Call remove() method on the private session attribute"""
         self.__session.remove()
 
     def get(self, cls, id):
-        """taking an object by class and id *"""
+        """
+        Retrieves an object by class and ID
+        """
         if cls is None or id is None:
             return None
+        if isinstance(cls, str):
+            cls = classes.get(cls, None)
+        if cls is None:
+            return None
         return self.__session.query(cls).get(id)
-    
+
     def count(self, cls=None):
-        """counting the number in storage"""
+        """
+        Returns the number of objects in storage matching the given class.
+        If no class is passed, returns the count of all objects in storage.
+        """
         if cls is None:
             return sum(len(self.__session.query(model).all()) for model in classes.values())
+        if isinstance(cls, str):
+            cls = classes.get(cls, None)
+        if cls is None:
+            return 0
         return len(self.__session.query(cls).all())
