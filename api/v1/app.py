edc1f05d93b1351785d3e17db0b97cbcd7e5b823
Abdelnissoul <abdelnissoul205@gmail.com>
2024-05-29 00:59:51 +0100

    get stats

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-"""Module containing app setup."""
-from flask import Flask, jsonify
+"""creating a flsak app"""
+
+from os import getenv
+from flask import Flask
+from models import storage
+from api.v1.views import app_views
 
 app = Flask(__name__)
 
-
-@app.errorhandler(404)
-def not_found(error):
-    """Handle 404 errors."""
-    return jsonify({'error': 'Not found'}), 404
-
+app.register_blueprint(app_views)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    HOST = getenv('HBNB_API_HOST', '0.0.0.0')
+    PORT = int(getenv('HBNB_API_PORT', 5000))
+    app.run(host=HOST, port=PORT, threaded=True)
