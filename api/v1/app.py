7fcc68468973b291bb216e45e661ff3fdcbf08a3
Abdelnissoul <abdelnissoul205@gmail.com>
2024-05-29 01:16:32 +0100

    jsonify

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,7 +2,7 @@
 """creating a flsak app"""
 
 from os import getenv
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 
@@ -10,7 +10,20 @@ app = Flask(__name__)
 
 app.register_blueprint(app_views)
 
+@app.teardown_appcontext
+def teardown_engine(exception):
+    """making and exception"""
+    storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """function for error handling err404"""
+    response = {"error": "Not found"}
+    return jsonify(response), 404
+
+
 if __name__ == '__main__':
     HOST = getenv('HBNB_API_HOST', '0.0.0.0')
     PORT = int(getenv('HBNB_API_PORT', 5000))
-    app.run(host=HOST, port=PORT, threaded=True)
+    app.run(debug=True, host=HOST, port=PORT, threaded=True)
