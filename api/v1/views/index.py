589de6e820f8b852ea55d424f60f70162e5fc28e
Abdelnissoul <abdelnissoul205@gmail.com>
2024-05-29 01:08:00 +0100

    index

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,8 +3,24 @@
 
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
 
 @app_views.route('/status')
 def api_status():
     """Returns the status of the API."""
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats')
+def get_stats():
+    """gets the stats by calling stats"""
+    stats = {
+        'amenities': storage.count('Amenity'),
+        'cities': storage.count('City'),
+        'places': storage.count('Place'),
+        'reviews': storage.count('Review'),
+        'states': storage.count('State'),
+        'users': storage.count('User')
+    }
+
+    return jsonify(stats)
